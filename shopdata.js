437321d25a1d99896e188034c79c813d677a6058
DhananjaySarathe <68105420+DhananjaySarathe@users.noreply.github.com>
2022-10-06 09:56:55 +0530

    Update shopdata.js

--- shopdata.js
+++ shopdata.js
@@ -13,7 +13,7 @@ function allShops() {
         city:"jabalpur",
         locality:"gokalpur",
         pincode:482051,
-        name:"JL Men's Saloon",
+        name:"JL Men's Salon",
         visitingHours:"9:30am to 9:30pm",
         location:"https://www.google.com/maps/place/23%C2%B011'23.8%22N+79%C2%B059'07.7%22E/@23.1899294,79.9832738,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x7e1cf2b739089b4e!8m2!3d23.1899294!4d79.9854625?hl=en",
         services:fshop1
